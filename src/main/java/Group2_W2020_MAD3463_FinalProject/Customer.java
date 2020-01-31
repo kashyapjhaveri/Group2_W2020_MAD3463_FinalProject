cbc92a84a251896ed69b1ad61604303578201bf1
shreemarella <mvsrikanth96@gmail.com>
2020-01-31 10:29:49 -0500

    Created person,customer class

--- src/main/java/Group2_W2020_MAD3463_FinalProject/Customer.java
+++ src/main/java/Group2_W2020_MAD3463_FinalProject/Customer.java
@@ -1,4 +1,16 @@
 package Group2_W2020_MAD3463_FinalProject;
 
-public class Customer {
+public class Customer
+{
+    private String houseNumber;
+    private String streetName;
+    private String city;
+    private String province;
+
+    public Customer(String houseNumber, String streetName, String city, String province) {
+        this.houseNumber = houseNumber;
+        this.streetName = streetName;
+        this.city = city;
+        this.province = province;
+    }
 }
--- src/main/java/Group2_W2020_MAD3463_FinalProject/Person.java
+++ src/main/java/Group2_W2020_MAD3463_FinalProject/Person.java
@@ -1,9 +1,38 @@
 package Group2_W2020_MAD3463_FinalProject;
 
+import java.time.LocalDate;
+
 abstract public class Person
 {
     private Integer id;
     private String firstName,lastName;
     private Gender gender;
+    private LocalDate birthDate;
+    private Integer age;
+    private String mobileNumber;
+    private String email;
+    private String userName;
+    private String password;
 
+    final private Byte key=5;
+
+    public Person() {
+    }
+
+    public Person(Integer id, String firstName, String lastName, Gender gender, LocalDate birthDate, Integer age, String mobileNumber, String email, String userName, String password) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.birthDate = birthDate;
+        this.age = age;
+        this.mobileNumber = mobileNumber;
+        this.email = email;
+        this.userName = userName;
+        this.password = (password);
+        calculateAge();
+    }
+
+    private void calculateAge() {
+    }
 }
