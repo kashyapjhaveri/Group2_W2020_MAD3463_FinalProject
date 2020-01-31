f14a827a1e86653300d227e5c0a6f8fcb6cfca8c
shreemarella <mvsrikanth96@gmail.com>
2020-01-30 22:00:58 -0500

    Initial Main function declaring

--- src/main/java/Group2_W2020_MAD3463_FinalProject/Group2MainClass.java
+++ src/main/java/Group2_W2020_MAD3463_FinalProject/Group2MainClass.java
@@ -1,7 +1,36 @@
 package Group2_W2020_MAD3463_FinalProject;
 
+import com.sun.deploy.security.SelectableSecurityManager;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Group2MainClass {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        System.out.println("Enter Your Choice:--");
+       try
+       {
+           Scanner input = new Scanner(System.in);
+           int choice=0;
+           do {
+               System.out.println("=========================");
+               System.out.println("--------CREATE-----------");
+               System.out.println("1.Person");
+               System.out.println("2.Vehicle");
+               System.out.println("-------------------------");
+               System.out.println("0.Exit");
+               System.out.println("==========================");
+               choice=input.nextInt();
+               if (int nextChoice=0){
+                   System.out.println("Enter Your Choice:--");
+               }
+               else
+
+
+               
+           }
+       }
 
     }
 }
