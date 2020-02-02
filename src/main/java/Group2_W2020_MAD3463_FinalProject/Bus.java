480946fb84b37da51879af64a97a5c9015d7c076
gagandeep kaur <sherry2312.gk@gmail.com>
2020-02-02 00:24:08 -0500

    declared variable

--- src/main/java/Group2_W2020_MAD3463_FinalProject/Bus.java
+++ src/main/java/Group2_W2020_MAD3463_FinalProject/Bus.java
@@ -1,3 +1,7 @@
 package Group2_W2020_MAD3463_FinalProject;
+public class Bus extends  Vehical{
+    private String typeOfBus;
+    private Boolean isAccessibilityAvailable;
+    private Boolean isWifiAvailable;
 
-
+   
