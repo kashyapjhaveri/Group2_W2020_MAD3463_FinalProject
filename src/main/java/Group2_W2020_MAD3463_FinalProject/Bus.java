53b3741a2e8d887d5c676f42cf956ec711f7a740
gagandeep kaur <sherry2312.gk@gmail.com>
2020-02-02 00:22:54 -0500

    bus class created

--- src/main/java/Group2_W2020_MAD3463_FinalProject/Bus.java
+++ src/main/java/Group2_W2020_MAD3463_FinalProject/Bus.java
@@ -1,37 +1,3 @@
 package Group2_W2020_MAD3463_FinalProject;
 
-public class Bus extends  Vehical{
-    private String typeOfBus;
-    private Boolean isAccessibilityAvailable;
-    private Boolean isWifiAvailable;
-
-    public String getTypeOfBus() {
-        return typeOfBus;
-    }
-
-    public void setTypeOfBus(String typeOfBus) {
-        this.typeOfBus = typeOfBus;
-
-    }
-    public Boolean getAccessibilityAvailable() {
-        return isAccessibilityAvailable;
-    }
-
-    public void setAccessibilityAvailable(Boolean accessibilityAvailable) {
-        isAccessibilityAvailable = accessibilityAvailable;
-    }
-    public Boolean getWifiAvailable() {
-        return isWifiAvailable;
-    }
-
-    public void setWifiAvailable(Boolean wifiAvailable) {
-        isWifiAvailable = wifiAvailable;
-    }
-
-    @Override
-    public void print() {
-
-    }
-}
-
 
