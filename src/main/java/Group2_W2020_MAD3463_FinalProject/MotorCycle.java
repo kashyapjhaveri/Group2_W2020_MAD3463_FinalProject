ff8f80f35d8fc5fc8b808235df2edc934c5e6e5b
gagandeep kaur <sherry2312.gk@gmail.com>
2020-02-02 00:45:03 -0500

    generated getter and setter

--- src/main/java/Group2_W2020_MAD3463_FinalProject/MotorCycle.java
+++ src/main/java/Group2_W2020_MAD3463_FinalProject/MotorCycle.java
@@ -3,4 +3,24 @@ package Group2_W2020_MAD3463_FinalProject;
 public class MotorCycle extends Vehical {
     private Integer maxTopSpeed;
     private float milage;
+
+    public Integer getMaxTopSpeed() {
+        return maxTopSpeed;
+    }
+
+    public void setMaxTopSpeed(Integer maxTopSpeed) {
+        this.maxTopSpeed = maxTopSpeed;
+    }
+
+    public float getMilage() {
+        return milage;
+    }
+
+    public void setMilage(float milage) {
+        this.milage = milage;
+    }
+    @Override
+    public void print() {
+
+    }
 }
