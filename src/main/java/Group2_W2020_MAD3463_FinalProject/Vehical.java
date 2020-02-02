a8b70c6fcf765170ff8f299b2acf1eb3a2fb4872
gagandeep kaur <sherry2312.gk@gmail.com>
2020-02-02 00:08:48 -0500

    variable declared

--- src/main/java/Group2_W2020_MAD3463_FinalProject/Vehical.java
+++ src/main/java/Group2_W2020_MAD3463_FinalProject/Vehical.java
@@ -1,6 +1,24 @@
 
 
 package Group2_W2020_MAD3463_FinalProject;
+abstract public class Vehical implements IDisplay
+{
+    private String VehicalIdentificationNumber ;
+    private String vechicalDiscription;
+    private String manufacturerName;
+    private Boolean isSelfDrive;
+    private Driver driver;//Object of driver assigned to the vehical
+    private Boolean isInsured;
+    private String
+            insuranceProviderName;
+    private Byte noOfSeat;
+    private Fuel fuelType;
+    private Integer baseRate;
+    private Integer ratePerKm;
+
+   
+
+}
 
 
 
