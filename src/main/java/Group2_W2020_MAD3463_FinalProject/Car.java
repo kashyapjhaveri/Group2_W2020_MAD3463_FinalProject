a34c6b9831732be7c42437d9cd0e43a641ee23a0
gagandeep kaur <sherry2312.gk@gmail.com>
2020-02-02 00:35:42 -0500

    generated getter and setter

--- src/main/java/Group2_W2020_MAD3463_FinalProject/Car.java
+++ src/main/java/Group2_W2020_MAD3463_FinalProject/Car.java
@@ -1,7 +1,22 @@
 package Group2_W2020_MAD3463_FinalProject;
 
-public class Car extends Vehical {
+public class Car extends  Vehical {
     private String carType;
     private String color;
-    
+
+    public String getCarType() {
+        return carType;
+    }
+
+    public void setCarType(String carType) {
+        this.carType = carType;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
 }
