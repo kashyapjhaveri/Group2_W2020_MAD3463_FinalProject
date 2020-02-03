d1a0bd7b121003b237a0a6dfd75233ffa3920dd4
shreemarella <mvsrikanth96@gmail.com>
2020-02-03 11:44:04 -0500

    data printing from Vehicle to MotorCycle

--- src/main/java/Group2_W2020_MAD3463_FinalProject/MotorCycle.java
+++ src/main/java/Group2_W2020_MAD3463_FinalProject/MotorCycle.java
@@ -33,7 +33,7 @@ public class MotorCycle extends Vehicle
 
     @Override
     public void print() {
-        /*System.out.println(getVehicalIndentificationNumber()+" "+getVechicalDiscription()+" "+getManufacturerName()+" "+getSelfDrive()+" "+getInsured()+" "+getInsuranceProviderName()+" "+getNoOfSeat()+" "+getFuelType()+" "+getBaseRate()+" "+getRatePerKm()+" "+getMaxTopSpeed()+" "+getMaxTopSpeed());*/
+        System.out.println(getVehicleIdentificationNumber()+" "+getVehicleDescription()+" "+getManufacturerName()+" "+getSelfDrive()+" "+getInsured()+" "+getInsuranceProviderName()+" "+getNoOfSeat()+" "+getFuelType()+" "+getBaseRate()+" "+getRatePerKm()+" "+getMaxTopSpeed()+" "+getMaxTopSpeed());
         System.out.println("VIn :-" + getVehicleIdentificationNumber());
         System.out.println("Description :-" + getVehicleDescription());
         System.out.println("Manufacturer :-" + getManufacturerName());
