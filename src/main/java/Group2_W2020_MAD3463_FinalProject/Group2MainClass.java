79040bdb19d870d1a4e972b72a042a9a175d0321
gagandeep kaur <sherry2312.gk@gmail.com>
2020-02-03 00:30:58 -0500

    code

--- src/main/java/Group2_W2020_MAD3463_FinalProject/Group2MainClass.java
+++ src/main/java/Group2_W2020_MAD3463_FinalProject/Group2MainClass.java
@@ -51,3 +51,38 @@ public class Group2MainClass {
 }
     public static void readBusesToHashMap()
 
+    {
+        String line="";
+        try{
+            File file = new File(".\\InputFiles\\Buses.csv");
+            FileReader reader = new FileReader(file);
+            BufferedReader bufferedReader=new BufferedReader(reader);
+            while ((line=bufferedReader.readLine())!=null)
+            {
+                String content[]=line.split(",");
+                Driver tempDriver = null;
+
+                for (int j=0;j<content.length;j++)
+                {
+                    Fuel tempFuel = null;
+
+                    if (content[8].equalsIgnoreCase("Petrol"))
+                        tempFuel=Fuel.petrol;
+                    else if (content[8].equalsIgnoreCase("Diesel"))
+                        tempFuel=Fuel.diesel;
+
+                    if (content[3].equalsIgnoreCase("false"))
+                    {
+                        tempDriver=drivers.get(content[4]);
+                    }
+
+                    Bus tempObj = new Bus(content[0],content[1],content[2],Boolean.parseBoolean(content[3]),tempDriver,Boolean.parseBoolean(content[5]),content[6],Byte.parseByte(content[7]),tempFuel,Integer.parseInt(content[9]),Integer.parseInt(content[10]),content[11],Boolean.parseBoolean(content[12]),Boolean.parseBoolean(content[13]));
+                    buses.put(content[0],tempObj);
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            System.out.println(e);
+        }
+    }
